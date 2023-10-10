c4a42ca07f630e0006698ef147df0aa3840d06a8
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2023-10-10 23:27:16 +0300

    added npm:jquery, migration:softDeletes, tabs to template lk.index

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
+window.$ = window.jQuery = require('jquery');
 import 'bootstrap';
+
+$(document).ready(function () {
+    $('#lkTabs a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+        console.log(e.target)
+        $('#lkTabs a').addClass('text-black-50');
+        $(e.target).removeClass('text-black-50');
+    });
+});
--- resources/views/lk/index.blade.php
+++ resources/views/lk/index.blade.php
@@ -9,31 +9,72 @@
             {{ __('lk.add_ad') }}
         </a>
     </p>
-    @if (count($bbs) > 0)
-        <table class="table table-striped table-borderless">
-            <thead>
-            <tr>
-                <th>{{ __('bbs.title') }}</th>
-                <th>{{ __('bbs.price') }}, {{ __('bbs.rub') }}.</th>
-                <th>{{ __('lk.created') }}</th>
-                <th>{{ __('lk.actions') }}</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($bbs as $bb)
-                <tr>
-                    <td><h3>{{ $bb->title }}</h3></td>
-                    <td>{{ $bb->price }}</td>
-                    <td>{{ $bb->created_at->locale('ru')->isoFormat('LLL') }}</td>
-                    <td>
-                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                            <a class="btn btn-sm btn-outline-secondary" href="{{ route('lk.edit', $bb) }}">{{ __('lk.change') }}</a>
-                            <a class="btn btn-sm btn-outline-danger" href="{{ route('lk.delete', $bb) }}">{{ __('lk.delete') }}</a>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    @endif
+    <!-- Горизонтальные табы -->
+    <ul class="nav nav-tabs" id="lkTabs" role="tablist">
+        <li class="nav-item" role="presentation">
+            <a
+                class="nav-link active"
+                id="list-bbs-tab"
+                data-bs-toggle="tab"
+                href="#list-bbs"
+                role="tab"
+                aria-controls="list-bbs"
+                aria-selected="true"
+            >
+                Активные объявления
+            </a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a
+                class="nav-link text-black-50"
+                id="deleted-bbs-tab"
+                data-bs-toggle="tab"
+                href="#deleted-bbs"
+                role="tab"
+                aria-controls="deleted-bbs"
+                aria-selected="false"
+            >
+                Удаленные объявления
+            </a>
+        </li>
+    </ul>
+    <!-- Содержимое табов -->
+    <div class="tab-content" id="lkTabsContent">
+        <!-- Таб 1: Список объявлений -->
+        <div class="tab-pane fade show active" id="list-bbs" role="tabpanel" aria-labelledby="list-bbs-tab">
+            @if (count($bbs) > 0)
+                <table class="table table-striped table-borderless">
+                    <thead>
+                    <tr>
+                        <th>{{ __('bbs.title') }}</th>
+                        <th>{{ __('bbs.price') }}, {{ __('bbs.rub') }}.</th>
+                        <th>{{ __('lk.created') }}</th>
+                        <th>{{ __('lk.actions') }}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($bbs as $bb)
+                        <tr>
+                            <td><h3>{{ $bb->title }}</h3></td>
+                            <td>{{ $bb->price }}</td>
+                            <td>{{ $bb->created_at->locale('ru')->isoFormat('LLL') }}</td>
+                            <td>
+                                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                                    <a class="btn btn-sm btn-outline-secondary" href="{{ route('lk.edit', $bb) }}">{{ __('lk.change') }}</a>
+                                    <a class="btn btn-sm btn-outline-danger" href="{{ route('lk.delete', $bb) }}">{{ __('lk.delete') }}</a>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            @endif
+        </div>
+        <!-- Таб 2: Список удаленных объявлений -->
+        <div class="tab-pane fade" id="deleted-bbs" role="tabpanel" aria-labelledby="deleted-bbs-tab">
+            <!-- Здесь разместите содержимое для второй вкладки -->
+            <h3>Список удаленных объявлений</h3>
+            <!-- Ваш список удаленных объявлений и другой контент здесь -->
+        </div>
+    </div>
 @endsection
